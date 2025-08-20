e102a5c6e0bde909b2fc3109443da1b2b3260bb7
Girlie Razon <girlie.razon84@gmail.com>
2025-08-21 00:44:52 +0200

    Add ApplyMiddlewares configuration with CORS, Helmet, and error handling

---
+++ server/src/config/ApplyMiddlewares.ts
@@ -0,0 +1,30 @@
+import cors from 'cors'
+import express from 'express'
+import helmet from 'helmet'
+import { errorHandler } from '../middlewares/Middleware'
+import MorganMiddleware from  '../middlewares/MorganMiddleware'
+
+
+// Middlewares
+const allowedOrigins = ['http://localhost:3000']
+
+// const allowedOrigins = ['*']
+const allowedMethods = ['GET', 'POST', 'PUT', 'DELETE']
+
+const options: cors.CorsOptions = {
+    origin: allowedOrigins,
+    methods: allowedMethods,
+    credentials: true,
+};
+
+const ApplyMiddlewares = (app: express.Application) => {
+    app.use(cors(options));
+    app.use(helmet());
+    app.use(express.urlencoded({extended: false}))
+    app.use(express.json())
+    app.use(MorganMiddleware);
+    app.use(errorHandler);
+};
+
+export default ApplyMiddlewares;
+export { allowedOrigins, allowedMethods, options };
