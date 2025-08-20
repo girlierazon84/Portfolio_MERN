214fddf3436cb9632bfd05cfc891f9ae45ee4660
Girlie Razon <girlie.razon84@gmail.com>
2025-08-21 00:14:04 +0200

    Add StatusCode constants for standardized HTTP response codes

---
+++ server/src/config/StatusCode.ts
@@ -0,0 +1,21 @@
+const OK = 200
+const CREATED = 201
+const ACCEPTED = 202
+const NO_CONTENT = 204
+const BAD_REQUEST = 400
+const UNAUTHORIZED = 401
+const FORBIDDEN = 403
+const NOT_FOUND = 404
+const INTERNAL_SERVER_ERROR = 500
+
+export default {
+    OK,
+    CREATED,
+    ACCEPTED,
+    NO_CONTENT,
+    BAD_REQUEST,
+    UNAUTHORIZED,
+    FORBIDDEN,
+    NOT_FOUND,
+    INTERNAL_SERVER_ERROR
+}
