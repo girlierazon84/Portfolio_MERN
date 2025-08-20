ed544158d191c3572585519067465e0afa3d49a5
Girlie Razon <girlie.razon84@gmail.com>
2025-08-21 01:08:37 +0200

    Add UserModel schema for user data management

---
+++ server/src/models/UserModel.ts
@@ -0,0 +1,18 @@
+import { Schema, model } from 'mongoose'
+import { CreateNewUser } from '../utils/interfaces/Users'
+
+const dbCollection = process.env.MONGODB_COLLECTION_USER ?? 'users';
+
+const newUserSchema = new Schema<CreateNewUser>({
+    firstname: { type: String, required: true },
+    lastname: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
+},
+    {timestamps: true}
+)
+
+const UserModel = model<CreateNewUser>(dbCollection, newUserSchema);
+
+export default UserModel;
