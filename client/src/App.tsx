29099c4ed7e4f6b3744274554b6a29336eb6d12b
Girlie Razon <girlie.razon84@gmail.com>
2025-08-20 23:23:30 +0200

    Add initial package.json for server setup with dependencies and scripts

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Pic03 from './utils/images/Pic03.png';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My Portfolio</h1>
+        <img src={Pic03} className="App-image" alt="image" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
