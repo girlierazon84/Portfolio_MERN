f2697cb063e75c755a63a3ee4a976fab4f524786
Girlie Razon <girlie.razon84@gmail.com>
2025-08-21 02:33:09 +0200

    Refactor UserModel to consolidate schema definition and remove unused variables

--- server/src/models/UserModel.ts
+++ server/src/models/UserModel.ts
@@ -1,20 +1,19 @@
 import { Schema, model } from 'mongoose'
 import { CreateNewUser } from '../utils/interfaces/Users'
 
+const userSchema = new Schema<CreateNewUser>({
+  firstname: { type: String, required: true },
+  lastname: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: {
+    type: String,
+    enum: ['user', 'admin'],
+    default: 'user'
+  }
+}, { timestamps: true })
 
-const newUserSchema = new Schema<CreateNewUser>({
-    firstname: { type: String, required: true },
-    lastname: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    username: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
-},
-    {timestamps: true}
-)
+const UserModel = model<CreateNewUser>('users', userSchema)
 
-// Define the collection name for the users
-const dbCollection = 'users';
-
-const UserModel = model<CreateNewUser>(dbCollection, newUserSchema);
-
-export default UserModel;
+export default UserModel
