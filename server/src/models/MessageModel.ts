7233d2b1d7d8036da22b005bf6f06fe96afa8fbb
Girlie Razon <girlie.razon84@gmail.com>
2025-08-21 02:09:08 +0200

    Refactor MessageModel schema to include status field with controlled values

--- server/src/models/MessageModel.ts
+++ server/src/models/MessageModel.ts
@@ -2,17 +2,21 @@ import { Schema, model } from 'mongoose'
 import { ContactMessage } from '../utils/interfaces/Message'
 
 const newMessageSchema = new Schema<ContactMessage>({
-    name: { type: String, required: true },
-    email: { type: String, required: true },
-    subject: { type: String, required: true },
-    message: { type: String, required: true },
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  subject: { type: String, required: true },
+  message: { type: String, required: true },
+  status: {
+    type: String,
+    enum: ['pending', 'read', 'responded'], // controlled values
+    default: 'pending'
+  }
 },
-    {timestamps: true}
-)
+{ timestamps: true })
 
-// Define the collection name for the messages
-const dbCollection = 'messages';
+// Define the collection name
+const dbCollection = 'messages'
 
-const MessageModel = model<ContactMessage>(dbCollection, newMessageSchema);
+const MessageModel = model<ContactMessage>(dbCollection, newMessageSchema)
 
-export default MessageModel;
+export default MessageModel
